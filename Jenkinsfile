e16e4b7d6b527d98ff1238fed6e89601839010d9
bguha <bitan.guha.2501@gmail.com>
2022-05-07 01:09:43 +0530

    added

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    
+    tools {
+        terraform 'teraform001'
+    }
+    stages {
+        stage ("terraform init") {
+            steps {
+                sh 'terraform init'
+            }
+        }
+        stage ("terraform Action") {
+            steps {
+                echo "Terraform action is -->${action}"
+                sh ('terraform apply --auto-approve')
+            }
+        }
+    }
+}
+        
---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "bitan.terraform-tutorials.com"
+  acl    = "public-read"
+  #policy = file("Policy.json")
+  
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+  tags = {
+    Environment = "Terraform"
+    Name        = "my-tag"
+  }
+
+}
+
+resource "aws_s3_bucket_object" "index" {
+bucket = aws_s3_bucket.mybucket.id
+acl = "public-read" # or can be “public-read”
+key = "index.html"
+source = "./index.html"
+etag = filemd5("./index.html")
+content_type = "text/html"
+}
+resource "aws_s3_bucket_object" "error" {
+bucket = aws_s3_bucket.mybucket.id
+acl = "public-read" # or can be “public-read”
+key = "error.html"
+source = "./error.html"
+etag = filemd5("./error.html")
+content_type = "text/html"
+}
