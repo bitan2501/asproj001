be48523840f6aee9f6b2a4d02a06621a5dab7b29
bguha <bitan.guha.2501@gmail.com>
2022-05-10 19:06:36 +0530

    changed

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_s3_bucket" "mybucket01" {
   bucket = "www.${var.bucket_name}"
   acl    = "public-read"
-  #policy = file("Policy.json")
+  policy = file("Policy.json")
   
 
   website {
@@ -182,4 +182,27 @@ resource "aws_cloudfront_distribution" "root_s3_distribution" {
   }
 
   tags = var.common_tags
+}
+resource "aws_route53_record" "root-a" {
+  zone_id = aws_route53_zone.bitanawsproj.zone_id
+  name = var.domain_name
+  type = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www-a" {
+  zone_id = aws_route53_zone.bitanawsproj.zone_id
+  name = "www.${var.domain_name}"
+  type = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.www_s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.www_s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
