51cbf188d9b1d656b822875d98812dcfa1f6069d
bguha <bitan.guha.2501@gmail.com>
2022-05-10 14:24:33 +0530

    changed

--- main.tf
+++ main.tf
@@ -85,9 +85,3 @@ resource "aws_acm_certificate_validation" "example" {
   certificate_arn         = aws_acm_certificate.example.arn
   validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
 }
-
-resource "aws_lb_listener" "example" {
-  # ... other configuration ...
-
-  certificate_arn = aws_acm_certificate_validation.example.certificate_arn
-}
