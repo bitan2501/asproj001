a80103e8988622e6d2c8504fef8ab338f3fde0df
bguha <bitan.guha.2501@gmail.com>
2022-05-10 21:30:46 +0530

    changed

--- providers.tf
+++ providers.tf
@@ -11,12 +11,12 @@ terraform {
   backend "s3" {
     bucket = "yourdomain-terraform"
     key = "prod/terraform.tfstate"
-    region = "eu-west-1"
+    region = "eu-east-1"
   }
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-east-1"
 }
 
 provider "aws" {
