a6dcb1a4406a051aa23e915838c9895b478be06c
bitan2501 <bitan.guha.2501@gmail.com>
2022-05-10 10:56:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "mybucket" {
+resource "aws_s3_bucket" "mybucket01" {
   bucket = "bitanawsproj.online"
   acl    = "public-read"
-  policy = file("Policy.json")
+  #policy = file("Policy.json")
   
 
   website {
@@ -14,13 +14,29 @@ resource "aws_s3_bucket" "mybucket" {
   }
   tags = {
     Environment = "Terraform"
-    Name        = "my-tag"
+    Name        = "my-tag01"
+  }
+
+}
+resource "aws_s3_bucket" "mybucket02" {
+  bucket = "www.bitanawsproj.online"
+  acl    = "public-read"
+  #policy = file("Policy.json")
+  
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+  tags = {
+    Environment = "Terraform"
+    Name        = "my-tag02"
   }
 
 }
 
 resource "aws_s3_bucket_object" "index" {
-bucket = aws_s3_bucket.mybucket.id
+bucket = aws_s3_bucket.mybucket01.id
 acl = "public-read" # or can be “public-read”
 key = "index.html"
 source = "./index.html"
@@ -28,7 +44,23 @@ etag = filemd5("./index.html")
 content_type = "text/html"
 }
 resource "aws_s3_bucket_object" "error" {
-bucket = aws_s3_bucket.mybucket.id
+bucket = aws_s3_bucket.mybucket01.id
+acl = "public-read" # or can be “public-read”
+key = "error.html"
+source = "./error.html"
+etag = filemd5("./error.html")
+content_type = "text/html"
+}
+resource "aws_s3_bucket_object" "index" {
+bucket = aws_s3_bucket.mybucket02.id
+acl = "public-read" # or can be “public-read”
+key = "index.html"
+source = "./index.html"
+etag = filemd5("./index.html")
+content_type = "text/html"
+}
+resource "aws_s3_bucket_object" "error" {
+bucket = aws_s3_bucket.mybucket02.id
 acl = "public-read" # or can be “public-read”
 key = "error.html"
 source = "./error.html"
